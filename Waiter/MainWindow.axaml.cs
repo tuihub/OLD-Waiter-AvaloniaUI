e857192c03f8839fa0f5a1782d9d3433885b5d33
Tabing010102 <tabing010102@outlook.com>
2023-02-23 11:40:53 +0800

    vs Avalonia UI init

---
+++ Waiter/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace Waiter
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
