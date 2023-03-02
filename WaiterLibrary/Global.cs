1485c9ee870dc4774a35ab8c34f01cfc24ce2f44
Tabing010102 <tabing010102@outlook.com>
2023-03-02 17:16:13 +0800

    update WaiterLibrary

---
+++ WaiterLibrary/Global.cs
@@ -0,0 +1,24 @@
+﻿using Grpc.Net.Client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WaiterLibrary
+{
+    public static class Global
+    {
+        private static GrpcChannel? _channel;
+
+        public static string ServerURL = "https://theam.gyx.moe";
+        public static GrpcChannel Channel
+        {
+            get
+            {
+                return _channel ??= GrpcChannel.ForAddress(ServerURL);
+            }
+            set { _channel = value; }
+        }
+    }
+}
---
+++ WaiterLibrary/Sephirah/Tiphereth/GetToken.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TuiHub.Protos.Librarian.Sephirah.V1;
+using static TuiHub.Protos.Librarian.Sephirah.V1.LibrarianSephirahService;
+
+namespace WaiterLibrary
+{
+    public static partial class Sephirah
+    {
+        public static async Task GetTokenAsync(string username, string password)
+        {
+            var client = new LibrarianSephirahServiceClient(Global.Channel);
+            await client.GetTokenAsync(new GetTokenRequest
+            {
+                Username = username,
+                Password = password
+            });
+        }
+    }
+}
